ad3bcd9006515ea06017d344289281da08636f50
Mike Innes <mike.j.innes@gmail.com>
2012-09-03 23:18:51 +0100

    undid

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.novemberain/monger "1.2.0"]
-                 #_[robbit "1.0.0-SNAPSHOT"]
+                 [robbit "1.0.0-SNAPSHOT"]
                  [com.draines/postal "1.8.0"]])
