986db611abbb38d03358d868454c3520a938f69d
Mike Innes <mike.j.innes@gmail.com>
2012-09-04 01:58:23 +0100

    added robbit deps

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.novemberain/monger "1.2.0"]
                  ; [robbit "1.0.0-SNAPSHOT"]
-                 [com.draines/postal "1.8.0"]])
+                 [com.draines/postal "1.8.0"]
+                 ; Robbit deps
+                 [clj-http            "0.5.2"]
+                 [cheshire            "4.0.1"]
+                 [clj-time            "0.4.3"]])
