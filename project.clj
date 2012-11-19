efdb6f564d54fa3ca8750f60c414d6735de97fb2
Mike Innes <mike.j.innes@gmail.com>
2012-11-19 07:33:48 +0000

    set lein version to 2.0.0

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src" "robbit"]
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.novemberain/monger "1.2.0"]
                  [enlive              "1.0.1"]
