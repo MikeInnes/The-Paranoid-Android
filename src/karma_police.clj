ccf4d004d1245a872a1b23d62a1b150d314481ab
Mike Innes <mike.j.innes@gmail.com>
2012-12-23 00:52:00 +0000

    new time-since-last-post functionality

--- src/karma_police.clj
+++ src/karma_police.clj
@@ -6,14 +6,14 @@
 (defn username [] (-> karma-police :login :name))
 
 (defn reposts [url]
-  (map link-from-url (repost-urls url)))
+  (map get-link (repost-urls url)))
 
 (defn top-comments [links]
   (->> links
-       (map first-reply)
+       (map #(-> % :replies first))
        (filter identity)
-       (remove deleted-comment?)
-       (remove #(author? % (username)))))
+       (filter #(> (:score %) 10))
+       (remove deleted-comment?)))
 
 (defn top-comment [links]
   (->> links top-comments
@@ -30,6 +30,8 @@
               (italic
                 (str "~ " (hyperlink author permalink)))))
 
+;; Number of times posted.
+
 (defn count-string [n]
   (condp = n
     1 "once"
@@ -37,25 +39,51 @@
     3 "thrice"
     (str n " times")))
 
+;; Time since last post
+
+(defn min-div [n & ds]
+  (int (Math/ceil (/ n (apply * ds)))))
+(defn days     [t] (min-div t 24 60 60))
+(defn hours    [t] (min-div t    60 60))
+(defn minutes  [t] (min-div t       60))
+(defn seconds  [t] (min-div t         ))
+
+(defn time-str [t]
+  (if-not (> (days t) 1)
+    (->> '[hours minutes seconds]
+         (map #(let [n ((eval %) t)]
+                 (if (> n 1) (str n " " %))))
+         (some identity))))
+
+(defn time-since-last-post [original reposts]
+  (let [now (.getTime (java.util.Date.))
+        ts  (->> reposts
+                 (filter #(= (:subreddit %) (:subreddit original)))
+                 (map #(- now (-> % :time .getTime)))
+                 (map #(/ % 1000)))]
+    (if (seq ts)
+      (time-str (apply min ts)))))
+
 (defn link-reply [url]
   (when-let [reposts (-> url reposts seq)]
     (when-let [top-comment (top-comment reposts)]
-      {:reply (paragraphs
-                (format-comment top-comment)
-                line
-                (italic
-                  (superscript-n 2
-                    "This image has been submitted "
-                    (hyperlink (count-string (count reposts)) (karmadecay-url url))
-                    " before - above is the previous top comment."))
-                (if (bot-post? top-comment)
-                  "Come on, people, this is just getting ridiculous."))
+      {:reply (p (format-comment top-comment)
+                 ---
+                 (i (sn 2 "This image has been submitted "
+                           (l (count-string (count reposts))
+                              (karmadecay-url url))
+                           " before"
+                           (if-let [t (time-since-last-post (get-link url) reposts)]
+                             (str " (and less than " t " ago - stay classy, OP)"))
+                           ". Above is the previous top comment."))
+                 (if (bot-post? top-comment)
+                   "Come on, people, this is just getting ridiculous."))
       :vote :up})))
 
 (def karma-police
   {:handler      (comp link-reply :permalink)
    :user-agent   "Top Comment Bot by /u/one_more_minute"
-   :subreddits   ["funny" "wtf" "pics" "gifs"]
+   :subreddits   ["funny" "wtf" "pics"]
    :type         :link
    :login        users/trapped-in-robot
    ; :log          (comp println str)
