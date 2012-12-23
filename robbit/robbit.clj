1ec6faa9af0494408bcccc095c25a66bdbfe85a1
Mike Innes <mike.j.innes@gmail.com>
2012-12-23 00:51:30 +0000

    robbit changes

--- robbit/robbit.clj
+++ robbit/robbit.clj
@@ -1,26 +1,13 @@
 (ns robbit
-  (:use     [reddit :exclude (author?)]
+  "A framework for running reddit bots."
+  (:use      robbit.log robbit.response
+            [reddit :exclude (author?)]
              util.spacers)
   (:require [reddit.url :as url]))
 
-(defn now [] (java.util.Date.))
+;; NEEDS TO BE DELETED AND REWRITTEN COMPLETELY
 
-;; ------------------
-;; Logging functions.
-;; ------------------
-
-(def map' (comp dorun map))
-
-(defonce log-str (atom ""))
-
-(defn log [& s] (->> (swap! log-str #(str (apply str s)
-  "\n----------------------------------------------------\n"
-                                          %))
-                     (spit "log.txt")))
-
-(defn load-log [] (reset! log-str (slurp "log.txt")))
-
-(defn reset-log [] (reset! log-str ""))
+(defn- now [] (java.util.Date.))
 
 (defonce bots (atom {}))
 
@@ -29,12 +16,12 @@
   this object."
   []
   {:user-agent   "Unnamed bot made with robbit."  ; Short description + main account username.
-   :type         :comment                         ; Respond to comments or links.
+   :type         :comment                         ; :comment/:link - choose which to respond to.
    :handler      (fn [_] nil)                     ; fn to take a comment object and return a response map.
    :subreddits   "all"                            ; String or vector of strings. Load comments/links from here.
    :login        nil                              ; Use reddit/login to generate a login.
    :interval     5                                ; Minutes between successive runs.
-   :last-run     (now)                ; Date in the format "yyyy-MM-dd HH:mm:ss".
+   :last-run     (now)                            ; Date in the format "yyyy-MM-dd HH:mm:ss".
    :debug        false                            ; Responses will be logged but not actually performed.
    :log          log
    :cancelled    (atom false)})                   ; Internal: the bot's thread will check this.
@@ -52,39 +39,12 @@
 (defn update-last-run [bot date]
   (swap! (bot :last-run) #(if (.after date %) date %)))
 
-(defmulti handle-response
-  "This is applied to each pair of the response map, dispatching
-  based on key. eg if the response map contains `{:reply string}`,
-  `(handle-response bot item string) is called, dispatching to
-  :reply."
-  (fn [bot item data] (:response-type (meta item))))
-
-(defmethod handle-response :reply [bot item text]
-  (let [result (if-not (bot :debug)
-                 (reply item text (bot :login))
-                 :debug)]
-    (update-last-run bot (item :time))
-    ((bot :log)
-      (item :permalink) "\n"
-      (item (if (comment? item) :body :title)) "\n"
-      "\n"
-      "Reply by " (-> bot :login :name) ":\n"
-      text "\n"
-      result)))
-
-(defmethod handle-response :vote  [bot item direction]
-  (when-not (bot :debug)
-    (vote item direction (bot :login))))
-
 (defn bot-items [bot]
   (->> (bot :subreddits)
        ((if (= (bot :type) :link) url/subreddit-new
                                   url/subreddit-comments))
        (items-since @(bot :last-run))
        (filter #(not (author? % bot)))))
-
-(defn print-log []
-  (println @log-str))
 
 ;; ------------------------
 ;; User-friendly functions.
