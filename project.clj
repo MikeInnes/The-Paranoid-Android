d31c41e118bef95821b691fb5e951689fc5fcd6a
Mike Innes <mike.j.innes@gmail.com>
2013-07-13 21:04:35 +0100

    clean up robbit dependancy

--- project.clj
+++ project.clj
@@ -1,15 +1,7 @@
 (defproject bots "0.1.0-SNAPSHOT"
-  :description ""
-  :url ""
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src" #_"robbit"]
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.novemberain/monger "1.2.0"]
                  [enlive              "1.0.1"]
-                 [robbit "1.0.0-SNAPSHOT"]
-                 ; Robbit deps
-                 #_[clj-http            "0.5.2"]
-                 #_[cheshire            "4.0.1"]
-                 #_[clj-time            "0.4.3"]])
+                 [robbit "1.0.0"]
+                 [one_more_minute/clarity "0.1.0"]])
