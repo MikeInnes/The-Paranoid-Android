b33e33b0192c2b84a06fcb1ea720fcec661fcec3
Mike Innes <mike.j.innes@gmail.com>
2013-07-13 21:05:08 +0100

    move to clarity

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bots "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.novemberain/monger "1.2.0"]
                  [enlive              "1.0.1"]
                  [robbit "1.0.0"]
--- src/scp.clj
+++ src/scp.clj
@@ -1,66 +1,74 @@
 (ns scp
   "Replies to mentions of SCP-wiki articles with links, on /r/scp."
-  (:use reddit.format)
+  (:use reddit.format clarity.core)
   (:require  users
             [clojure.string  :as str]
             [clj-http.client :as http]))
 
-(def marvin-quotes
- ["I think you ought to know I'm feeling very depressed."
-  "I'd make a suggestion, but you wouldn't listen. No one ever does."
-  ; "I've been talking to the main computer. It hates me."
-  "I've calculated your chance of survival, but I don't think you'll like it."
-  "I have a million ideas, but, they all point to certain death."
-  "Now I've got a headache."
-  "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it oh God I'm so depressed."
-  "And then of course I've got this terrible pain in all the diodes down my left side."
-  "Do you want me to sit in a corner and rust or just fall apart where I'm standing?"
-  "The first ten million years were the worst. And the second ten million: they were the worst, too. The third ten million I didn't enjoy at all. After that, I went into a bit of a decline."
-  "It gives me a headache just trying to think down to your level."
-  "Life. Loathe it or ignore it. You can't like it."
-  "Funny, how just when you think life can't possibly get any worse it suddenly does."
-  ;; Not actual quotes.
-  "I've been talking to the reddit server. It hates me."
-  "Here I am, brain the size of a planet, posting links. Call that job satisfaction, 'cause I don't. "
-  "I would correct your grammar as well, but you wouldn't listen. No one ever does."])
+(use-clarity)
+(clarity
 
-(defn scp-url [n]
-  (str "http://scp-wiki.wikidot.com/scp-" n))
+def marvin-quotes
+  list
+    "I think you ought to know I'm feeling very depressed."
+    "I'd make a suggestion, but you wouldn't listen. No one ever does."
+    ; "I've been talking to the main computer. It hates me."
+    "I've calculated your chance of survival, but I don't think you'll like it."
+    "I have a million ideas, but, they all point to certain death."
+    "Now I've got a headache."
+    "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it oh God I'm so depressed."
+    "And then of course I've got this terrible pain in all the diodes down my left side."
+    "Do you want me to sit in a corner and rust or just fall apart where I'm standing?"
+    "The first ten million years were the worst. And the second ten million: they were the worst, too. The third ten million I didn't enjoy at all. After that, I went into a bit of a decline."
+    "It gives me a headache just trying to think down to your level."
+    "Life. Loathe it or ignore it. You can't like it."
+    "Funny, how just when you think life can't possibly get any worse it suddenly does."
+    ;; Not actual quotes.
+    "I've been talking to the reddit server. It hates me."
+    "Here I am, brain the size of a planet, posting links. Call that job satisfaction, 'cause I don't. "
+    "I would correct your grammar as well, but you wouldn't listen. No one ever does."
 
-(defn scp-link [n]
-  (hyperlink (str "SCP-" n) (scp-url n)))
+defn scp-url [n]
+  str "http://scp-wiki.wikidot.com/scp-" n
 
-(defn exists? [n]
-  (-> n scp-url (http/get {:throw-exceptions false}) :status (not= 404)))
+defn scp-link [n]
+  hyperlink (str "SCP-" n) (scp-url n)
 
-(defn remove-brackets [s]
-  (loop [s' ""
-         [first & rest] (map str s)
-         brackets 0]
+defn exists? [n]
+  -> n scp-url (http/get {:throw-exceptions false}) :status (not= 404)
+
+defn remove-brackets [s]
+  loop [s' ""
+        [first & rest] (map str s)
+        brackets 0]
     (cond
       (nil? first)       s'
       (#{"(" "["} first) (recur s' rest (inc brackets))
       (#{")" "]"} first) (recur s' rest (dec brackets))
       (> brackets 0)     (recur s' rest brackets)
-      :else              (recur (str s' first) rest brackets))))
+      :else              (recur (str s' first) rest brackets))
 
-(defn get-nums
+defn get-nums
   "Detects numbers 000-1999, including extensions."
-  [s] (re-seq #"(?i)(?x)              # Ignore case, comment mode
-                (?<! \d           )   # Not preceded by a digit
-                (?<! `            )   # Not preceded by `
-                1? \d{3}              # 000 - 1999
-                (?: -EX|-ARC|-J|-D)?  # Optional extensions
-                (?! `             )   # Not followed by a `
-                (?! \.\d          )   # Not followed by a decimal point"
-              (remove-brackets s)))
+  [s]
+  re-seq #"(?i)(?x)              # Ignore case, comment mode
+              (?<! \d           )   # Not preceded by a digit
+              (?<! `            )   # Not preceded by `
+              1? \d{3}              # 000 - 1999
+              (?: -EX|-ARC|-J|-D)?  # Optional extensions
+              (?! `             )   # Not followed by a `
+              (?! \.\d          )   # Not followed by a decimal point"
+         remove-brackets s
 
-(defn probably
+defn probably
   "True with probability n."
-  [n] (< (rand) n))
+  [n]
+  < (rand) n
 
-(defn scp-reply [text]
-  (when-let [nums (->> text get-nums distinct (filter exists?) seq)]
+def replies : atom [false {}]
+
+defn scp-reply [{:keys [body link_id]}]
+  when-let [nums (->> body get-nums distinct (filter exists?) #_(remove #(repeat? % link_id)) seq)]
     {:reply (paragraphs
               (str (str/join ", " (map scp-link nums)) ".")
                 (cond
@@ -69,12 +77,14 @@
                          "Brain the size of a planet, and this is what they've got me doing...")
                   (probably 1/10)
                     (rand-nth marvin-quotes)))
-     :vote  :up}))
+     :vote  :up}
 
-(def scp-bot
-  {:handler      (comp scp-reply :body)
+def scp-bot
+  {:handler      scp-reply
    :user-agent   "/r/scp helper by /u/one_more_minute"
    :subreddits   ["scp" "InteractiveFoundation" "SCP_Game"]
    :login        users/marvin
    :interval     0.5
-  })
+  }
+
+)
