3596f46078d973c4ed33d5ca91344198f894b03f
Mike Innes <mike.j.innes@gmail.com>
2014-08-10 19:44:29 +0100

    update deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject bots "0.1.0-SNAPSHOT"
+(defproject marvin "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.novemberain/monger "1.2.0"]
                  [enlive              "1.0.1"]
                  [mynx "2.0.0-SNAPSHOT"]
-                 [one_more_minute/clarity "0.1.1"]])
+                 [chiars "0.2.0-SNAPSHOT"]])
