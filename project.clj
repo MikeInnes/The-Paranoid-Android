de93c2e6c289eca0202c9bbd8bc16c53cca98633
Mike Innes <mike.j.innes@gmail.com>
2014-08-10 20:54:34 +0100

    update deps

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject marvin "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.novemberain/monger "1.2.0"]
-                 [enlive              "1.0.1"]
-                 [mynx "2.0.0-SNAPSHOT"]
+                 [enlive "1.1.5"]
                  [http-kit "2.1.18"]
+                 [mynx "2.0.0-SNAPSHOT"]
                  [chiara "0.2.0-SNAPSHOT"]])
